bdce68d2c4d539b749060b4c2abdb9693f601851
mo7amednasr72 <mohamednasr7222@gmail.com>
2025-04-15 14:44:07 +0200

    notes in program file

--- Blazor.Day02/Program.cs
+++ Blazor.Day02/Program.cs
@@ -12,7 +12,7 @@ namespace Blazor.Day02
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped<IService<Product>, ProductService>();
+            builder.Services.AddScoped<IService<Product>, ProductService>(); // note any request in agngular or blazor is a progressive request so if you didn't reload the page any scoped object (service) will be the same object untill you reload (refresh) the page even if you made any other request to the api to retrive any data (because it's a single page application not like MVC or API) in MVC or API any scoped object in them changes for each request 
             builder.Services.AddScoped<IService<Category>, CategoryService>();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
